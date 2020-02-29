59e6bb0825f16fb3e962ec2211eb80d2bc914cc8
Dahan Schuster <dan.plschuster@gmail.com>
2020-02-29 00:17:49 -0300

    Cria a classe Negociacao
    Cria a classe utilizada para representar uma Negociação
    Aplica conceitos de encapsulamento e congelamento

---
+++ client/js/app/model/Negociacao.js
@@ -0,0 +1,33 @@
+class Negociacao {  // jshint ignore:line
+	
+	/**
+	 * Negociação constructor
+	 *
+	 * @param data
+	 * @param quantidade
+	 * @param valor
+	 */
+	constructor({data, quantidade, valor}) {
+		this._data =  new Date(data ? data.getTime() : null) // instancia uma nova data, previnindo alteração da proprieadade por referência
+		this._quantidade = quantidade || 0
+		this._valor = valor || 0
+		Object.freeze(this)
+	}
+	
+	get data() {
+		return new Date(this._data.getTime()) // instancia uma nova data e retorna, previnindo alterações na propriedades do objeto Data da Negociação
+	}
+	
+	get quantidade() {
+		return this._quantidade
+	}
+	
+	get valor() {
+		return this._valor
+	}
+	
+	get volume() {
+		return this._quantidade * this._valor
+	}
+	
+}
