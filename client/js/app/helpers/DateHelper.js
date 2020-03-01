60e6d4fbf27be4a71ea268d355433ec05adc5f99
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-01 18:28:58 -0300

    Cria a classe helper para manipulação de datas

---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,16 @@
+class DateHelper {
+	
+	constructor() {
+		throw new Error('Classe DateHelper deve ser acessada estaticamente')
+	}
+	
+	static dataParaTexto = data => `${data.getDate()}/${(data.getMonth() + 1)}/${data.getFullYear()}`
+	
+	static textoParaData = texto =>{
+		if (!/^\d{4}-\d{2}-\d{2}$/.test(texto)) throw new Error('Texto deve estar no formato yyyy-mm-dd')
+		return new Date(...texto.split('-').map(
+			(value, i) => value - (i % 2),
+		))
+	}
+	
+}
