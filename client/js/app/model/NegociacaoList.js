0d6b083fa720f4905a390820369e7855e2587c67
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-01 18:29:51 -0300

    Cria a classe para listagem de objetos Negociacao

---
+++ client/js/app/model/NegociacaoList.js
@@ -0,0 +1,16 @@
+class NegociacaoList {
+	
+	constructor() {
+		this._negociacoes = []
+	}
+	
+	adicionar(negociacao) {
+		if (!(negociacao instanceof Negociacao)) throw new Error('Apenas instâncias de Negociacao são permitidas')
+		this._negociacoes.push(negociacao)
+	}
+	
+	get negociacoes(){
+		return new Array(...this._negociacoes);
+	}
+	
+}
