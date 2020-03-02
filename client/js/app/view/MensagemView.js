2a72e60ca359e77469ce2a07bacf0dace4996dda
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-01 22:21:04 -0300

    Aplica polimorfismo ao método _template da classe View

--- client/js/app/view/MensagemView.js
+++ client/js/app/view/MensagemView.js
@@ -1,7 +1,7 @@
 class MensagemView extends View {
 	
 	_template(mensagemModel) {
-		return `<p class="alert alert-info">${mensagemModel.texto}</p>`
+		return mensagemModel.texto ? `<p class="alert alert-info">${mensagemModel.texto}</p>` : ''
 	}
 	
 }
--- client/js/app/view/View.js
+++ client/js/app/view/View.js
@@ -1,20 +1,11 @@
 class View {
 	
 	constructor(elemento) {
-		// Para simular uma classe abstrata em JS, algumas medidas podem ser tomadas
-		
-		// Primeiro, validar que o construtor não é da classe View
+		// Validar que o construtor não é da classe View
 		// (uma classe filha, mesmo que não implemente um construtor
 		// explicitamente, terá um construtor vazio)
 		if (this.constructor === View) {
 			throw new Error('Classe "View" não pode ser instanciada')
-		}
-		
-		// Segundo, validar que a classe filha implementa os métodos
-		// exigidos para uma herança de View ─ no fundo, este é o
-		// objetivo de uma classe abstrata
-		if (this._template === undefined) {
-			throw new Error('A função "_template" deve ser implementada por classes View')
 		}
 		
 		// Se o código não lançou nenhum erro até agora, então
@@ -22,6 +13,10 @@ class View {
 		// necessários. Logo podemos continuar :)
 		
 		this._elemento = elemento
+	}
+	
+	_template() {
+		throw new Error('A função "_template" deve ser implementada por classes View')
 	}
 	
 	update(model) {
