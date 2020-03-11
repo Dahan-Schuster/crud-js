f1560eb58611564fe091b507cddf897547cbd0d8
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-10 23:12:00 -0300

    Atualiza a classe NegociacaoAjax para utilizar o AjaxHelper

--- client/js/app/services/Ajax/NegociacaoAjax.js
+++ client/js/app/services/Ajax/NegociacaoAjax.js
@@ -1,26 +1,17 @@
 class NegociacaoAjax {
 	
-	importarNegociacoesDaSemana(callbackFn) {
-		let xhr = new XMLHttpRequest()
-		xhr.open('GET', 'negociacoes/semana')
-		xhr.onreadystatechange = () => {
-			/**
-			 * 0: requisição ainda não iniciada.
-			 * 1: conexão com o servidor estabelecida.
-			 * 2: requisição recebida.
-			 * 3: processando requisição.
-			 * 4: requisição concluída e a resposta esta pronta.
-			 */
-			if (xhr.readyState === 4) {
-				if (xhr.status === 200) {
-					this._tratarResposta(JSON.parse(xhr.responseText), callbackFn)
-				} else {
-					console.log(xhr.responseText)
-					callbackFn('Não foi possível obter as negociações :(')
-				}
+	importarNegociacoesDaSemana(callbackFn, antesDeEnviar) {
+		AjaxHelper.ajax({
+			url: '/negociacoes/semana',
+			metodo: 'GET',
+			tipoRetorno: 'json',
+			antesDeEnviar,
+			sucesso: resposta => this._tratarResposta(resposta, callbackFn),
+			erro: erro => {
+				console.log(erro)
+				callbackFn('Não foi possível obter as negociações :(')
 			}
-		}
-		xhr.send()
+		})
 	}
 	
 	_tratarResposta = (negociacoes, callbackFn) => {
