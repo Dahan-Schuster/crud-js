614fefc7a99e2b7ed81c264fa46849231c68b7a7
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-11 21:53:17 -0300

    Melhora a classe NegociacaoAjax
    Para incrementar a legibilidade e manutenabilidade do código, foram
    feitas alterações na classe NegociacaoAjax para que os métodos retornem
    promises ao invés de receberem callbacks

--- client/js/app/services/Ajax/NegociacaoAjax.js
+++ client/js/app/services/Ajax/NegociacaoAjax.js
@@ -15,25 +15,16 @@ class NegociacaoAjax {
 		})
 	}
 	
-	importarNegociacoesDaSemana(callbackFn, antesDeEnviar) {
-		AjaxHelper.ajax({
-			url: '/negociacoes/semana',
-			metodo: 'GET',
-			tipoRetorno: 'json',
-			antesDeEnviar,
-			sucesso: resposta => this._tratarResposta(resposta, callbackFn),
-			erro: erro => {
-				console.log(erro)
-				callbackFn('Não foi possível obter as negociações :(')
-			}
+	importarNegociacoes(periodo = 'semana', antesDeEnviar = null) {
+		return new Promise((resolve, reject) => {
+			AjaxHelper.ajax({
+				url: `/negociacoes/${periodo}`,
+				metodo: 'GET',
+				tipoRetorno: 'json',
+				antesDeEnviar,
+				sucesso: resposta => resolve(resposta),
+				erro: erro => reject('Não foi possível obter as negociações :(')
+			})
 		})
-	}
-	
-	_tratarResposta = (negociacoes, callbackFn) => {
-		let negociacoesModel = negociacoes.map(
-			negociacao => new Negociacao({...negociacao, data: new Date(negociacao.data)}),
-		)
-		
-		callbackFn(null, negociacoesModel)
 	}
 }
