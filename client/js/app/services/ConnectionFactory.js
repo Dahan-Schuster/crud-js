0b0324517ba434e9e5c96f8e71548e3ff46521b8
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-15 00:11:18 -0300

    Cria a classe ConnectionFactory
    Cria a classe responsável por criar, configurar e retornar instâncias do
    objeto IndexedDB
    No commit anterior, um arquivo de aprendizado por enviado com código
    mal organizado, apesar de funcional, que cria instâncias do IDB, e salva
    e lista objetos no navegador. Este commit cria uma classe inteira que
    guarda os dados e o comportamento relacionados a criação e configuração
    do banco

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,63 @@
+const ConnectionFactory = (() => {
+	
+	const IDB_VERSAO = 1,
+		IDB_NOME = 'NegociacoesDb',
+		OBJECT_STORES = ['negociacoes']
+	
+	let connection, closeConnection
+	
+	return class ConnectionFactory {
+		
+		static getConnection() {
+			
+			return new Promise((resolve, reject) => {
+				let openDBRequest = window.indexedDB.open(IDB_NOME, IDB_VERSAO)
+				
+				openDBRequest.onupgradeneeded = configurarEstruturaDoBanco
+				openDBRequest.onsuccess = e => resolve(connection ? connection : novaConexao(e))
+				openDBRequest.onerror = e => {
+					console.log(e.target.error)
+					reject(e.target.error.name)
+				}
+			})
+			
+			/**
+			 * TODO: backup do banco antes de realizar a atualização
+			 * 1) fazer backup dos dados da Obj Storage em uma clone
+			 * 2) Recriar a Obj Storage original
+			 * 3) Tentar popular a nova Obj Storage com os dados antigos
+			 * 4) Deletar a Obj Storage de backup em caso de sucesso
+			 * 5) Informar a falha em caso de erro e manter o backup
+			 * *) Usar o envento IDBDatabase.onversionchange
+			 */
+			function configurarEstruturaDoBanco(e) {
+				let IDBDatabase = e.target.result
+				
+				OBJECT_STORES.forEach(objectStore => {
+					if (IDBDatabase.objectStoreNames.contains(objectStore)) {
+						IDBDatabase.deleteObjectStore(objectStore)
+					}
+					IDBDatabase.createObjectStore(objectStore, {autoIncrement: true})
+				})
+			}
+			
+			function novaConexao(e) {
+				connection = e.target.result
+				closeConnection = connection.close.bind(connection)
+				connection.close = function () {
+					throw new Error('A conexão é compartilhada e, portanto, não deve ser fechada')
+				}
+				return connection
+			}
+			
+		}
+		
+		static _closeConnection() {
+			if (connection) {
+				closeConnection()
+				connection = null
+			}
+		}
+		
+	}
+})()
