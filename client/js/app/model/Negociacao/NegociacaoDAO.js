cc81621fd7cb4a858912c5f53ddba6e13e15e0be
Dahan Schuster <dan.plschuster@gmail.com>
2020-03-17 16:08:08 -0300

    Cria o mótodo de listagem de todas as negociações cadastradas no banco

--- client/js/app/model/Negociacao/NegociacaoDAO.js
+++ client/js/app/model/Negociacao/NegociacaoDAO.js
@@ -10,6 +10,39 @@ class NegociacaoDAO extends DAO{
 	}
 	
 	/**
+	 * Pesquisa por todas as negociações salvas no banco
+	 * e retorna uma instância de NegociacaoList
+	 *
+	 * @return {Promise<NegociacaoList>}
+	 */
+	listarTodos() {
+		return new Promise((resolve, reject) => {
+			let ponteiro = this.iniciarTransacao().openCursor(),
+				negociacaoList = new NegociacaoList()
+			
+			ponteiro.onsuccess = e => {
+				let atual = e.target.result
+				if (atual !== null) {
+					let negociacao = atual.value
+					negociacaoList.adicionar(new Negociacao({
+						data: negociacao._data,
+						quantidade: negociacao._quantidade,
+						valor: negociacao._valor
+					}))
+					
+					atual.continue()
+				} else {
+					resolve(negociacaoList)
+				}
+			}
+			ponteiro.onerror = e => {
+				console.log(e.target.error)
+				reject('Não foi possível listar as negociações.')
+			}
+		})
+	}
+	
+	/**
 	 * Salva um objeto Negociacao no banco de dados
 	 * @param {Negociacao} negociacao
 	 * @returns {Promise}
@@ -24,4 +57,5 @@ class NegociacaoDAO extends DAO{
 			}
 		})
 	}
+
 }
